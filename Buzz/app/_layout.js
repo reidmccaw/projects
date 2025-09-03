13e57a16e906f0ff78e18af45ca1fd1bfcd4c3dd
reidmccaw <162385821+reidmccaw@users.noreply.github.com>
2025-09-02 18:46:37 -0700

    Delete Buzz directory

--- Buzz/app/_layout.js
+++
@@ -1,7 +0,0 @@
-import { Slot } from "expo-router";
-
-export default function SlotLayout() {
-  // Override default layout to ensure that our screen background bleeds
-  // into the status bar.
-  return <Slot />;
-}
